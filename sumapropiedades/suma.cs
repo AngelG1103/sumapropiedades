9266055f831f4d42ae71fec94df5e18a8dca0a52
Angel González <anngeeldavid@gmail.com>
2022-09-22 19:24:28 -0600

    Agregar archivos de proyecto.

---
+++ sumapropiedades/suma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumapropiedades
+{
+    class suma
+    {
+        private double v1;
+        private double v2;
+
+        public double V1 { get => v1; set => v1 = value; }
+        public double V2 { get => v2; set => v2 = value; }
+         public double sumar()
+        {
+            return V1 + V2;
+        }
+    }
+}
